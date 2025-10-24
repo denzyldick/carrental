c551ec8b81b63e13356e3313a2eba3190f2a3791
Denzyl Dick <ddenzyl@gmail.com>
2025-10-24 05:56:22 +0100

    Initial skeleton for client + api

---
+++ api/src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+final class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->json(["hello" => "world"]);
+    }
+}
